b1019ddb2c272770ef52d78099dddb5b71b3c4bf
Josh Long <josh@joshlong.com>
2024-04-16 10:01:19 +0200

    server

---
+++ server/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import com.example.demo.proto.HelloReply;
+import com.example.demo.proto.HelloRequest;
+import com.example.demo.proto.SimpleGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@GrpcService
+class GrpcServerService extends SimpleGrpc.SimpleImplBase {
+
+    @Override
+	public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+		HelloReply reply = HelloReply.newBuilder().setMessage("Hello ==> " + req.getName()).build();
+		responseObserver.onNext(reply);
+		responseObserver.onCompleted();
+    }
+
+}
