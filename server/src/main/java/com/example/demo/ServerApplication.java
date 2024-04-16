1df2a03ef0438e4f2f2628dd284993df582daf7a
Josh Long <josh@joshlong.com>
2024-04-16 10:28:32 +0200

    polish

--- server/src/main/java/com/example/demo/ServerApplication.java
+++ server/src/main/java/com/example/demo/ServerApplication.java
@@ -9,10 +9,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }
