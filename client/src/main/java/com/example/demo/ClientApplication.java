084086911fb19fe6291e80461b9dacfafa4d6aeb
Josh Long <josh@joshlong.com>
2024-04-16 11:53:11 +0200

    up

--- client/src/main/java/com/example/demo/ClientApplication.java
+++ client/src/main/java/com/example/demo/ClientApplication.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.proto.HelloRequest;
+import com.example.demo.proto.SimpleGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientApplication {
@@ -9,5 +14,15 @@ public class ClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
+
+	@Bean
+	ApplicationRunner clientRunner(@GrpcClient("greetingsService") SimpleGrpc.SimpleBlockingStub greetingService) {
+		return args -> {
+			var josh = HelloRequest.newBuilder().setName("Josh").build();
+			var helloReply = greetingService.sayHello(josh);
+			System.out.println(helloReply.toString());
+		};
+	}
+
 
 }
